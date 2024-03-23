ef4b2de0668e1d63f7cd5bfc5fbaea67b00e8447
mabrutsaunierVadim <vadimmabrutsaunier@gmail.com>
2024-03-23 15:32:04 +0100

    fonction age
    code de la fonction estMajeur

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,6 +1,18 @@
 //Fonctions réalisées par l'étudiant 1 :
 
+bool estMajeur(int age){
 
+	bool result;
+	if (age>=18){
+		result=true;
+	}
+	else if (0<age<18){
+		result=false;
+	}
+	else {
+		result=true;
+	}
+}
 
 
 
